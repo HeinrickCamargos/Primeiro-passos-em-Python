0b4ffcf265a62d7b3e736ab6412e7242400366e9
Heinrick <106637227+HeinrickCamargos@users.noreply.github.com>
2023-01-30 21:56:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+Autor: Heinrick Camargos
+Data: 30/01/2023    
+Versão: 1.0
+
+'''
+
+# Meu primeiro codigo em PY 
+
+nome = input('Qual é o seu nome: ')
+idade = input('Qual é o sua idade: ')
+idade = str(idade)
+cidade = input('Em qual cidade você mora: ')
+
+print ('O ' + nome + ' tem ' + idade + ' anos de idade e mora em ' + cidade + '.')
+
+
