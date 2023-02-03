557a401cbc826d1ccfaa8bee9b6738cee5e7e3da
Heinrick <106637227+HeinrickCamargos@users.noreply.github.com>
2023-02-03 21:20:51 +0000

    Nova atualização do bloco de notas

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ x = str(3)
 y = int(4)
 z = float (5)
 
-print (x = x)
-print (y = y)
-print (z = z)
+print (x + x)
+print (y - y)
+print (z + z)
 
 #
 
@@ -76,3 +76,64 @@ valor = str(valor)
 #index 01234
 
 print(valor[2:5])
+
+#metodos para string
+
+mensagem = 'Eu adoro comida caseira'
+
+print(mensagem.capitalize())
+print(mensagem.upper())
+print(mensagem.lower())
+print(mensagem.find('c'))
+print(mensagem.replace('caseira', 'feita em casa'))
+print(mensagem.strip())
+
+ # OPERADORES DE COMPARAÇÃO
+
+# == Equal -> vai fazer a comparação e ditar se e verdade ou falsa 
+
+operadores = 'banana' == 'Banana'
+print(operadores)
+
+operadores = 'banana' == 'banana'
+print(operadores)
+
+# != Not equal -> vai fazer a comparação e ditar se ela não e igual a outra
+
+operadores = 'banana' != 'Banana'
+print(operadores)
+
+operadores = 'banana' != 'banana' 
+print(operadores)
+
+# Greather than -> vai dizer se o resultado e maior que o outro !
+
+operadores = '10' > '25'
+print(operadores)
+
+operadores = '25' > '10'
+print(operadores)
+
+# < Less than -> vai dizer se o resultado e menor que o outro !
+
+operadores = '10' < '25'
+print(operadores)
+
+operadores = '25' < '10'
+print(operadores)
+
+# >= Greather than or equal to -> vai dizer se o resultado e maior ou igual que o outro
+
+operadores = '10' >= '25'
+print(operadores)
+
+operadores = '25' >= '10'
+print(operadores)
+
+# <= Greather than or equal to -> vai dizer se o resultado e menor ou igual que o outro
+
+operadores = '10' <= '25'
+print(operadores)
+
+operadores = '25' <= '10'
+print(operadores)
